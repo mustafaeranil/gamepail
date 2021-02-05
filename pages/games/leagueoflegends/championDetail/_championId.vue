3de8ba40b27965dc7eac6573fdba5a5798ff719e
mustafaeranil <mustafaeranil2@gmail.com>
2021-02-05 16:42:48 +0300

    champion detail page update

---
+++ pages/games/leagueoflegends/championDetail/_championId.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="container mx-auto">
+      <h1>TEST</h1>
+      <div v-for="champion in championData" :key="champion.id">
+        {{ champion.image.full }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters({
+      championData: 'game/leagueoflegends/championDetail/getChampionData',
+    }),
+  },
+  created() {
+    this.getChampionData(this.$route.params.championId)
+  },
+  methods: {
+    ...mapActions({
+      getChampionData: 'game/leagueoflegends/championDetail/getChampionData',
+    }),
+  },
+}
+</script>
