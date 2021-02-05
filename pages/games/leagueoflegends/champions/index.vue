3dc3de481addf4eaef5520882169078cb603cb28
mustafaeranil <mustafaeranil2@gmail.com>
2021-02-05 16:43:05 +0300

    Champions Page update

--- pages/games/leagueoflegends/champions/index.vue
+++ pages/games/leagueoflegends/champions/index.vue
@@ -1,27 +1,46 @@
 <template>
   <div>
-    <h1>Champions</h1>
-    <ul v-for="champion in champions.data" :key="champion.id">
-      <li>
-        <p>
-          {{ champion.name }} -- {{ champion.title }} // {{ champion.tags }}
-        </p>
-        <img
-          :src="
-            'http://ddragon.leagueoflegends.com/cdn/11.3.1/img/champion/' +
-            champion.id +
-            '.png'
-          "
-        />
-      </li>
-    </ul>
+    <div class="container mx-auto">
+      <h1>Champions</h1>
+      <ul v-for="champion in champions" :key="champion.id">
+        <li>
+          <NuxtLink
+            class="mb-4 flex"
+            :to="'/games/leagueoflegends/championDetail/' + champion.id"
+          >
+            <img
+              class="mr-4 w-10"
+              :src="
+                'http://ddragon.leagueoflegends.com/cdn/11.3.1/img/champion/' +
+                champion.id +
+                '.png'
+              "
+            />
+            <span
+              >{{ champion.name }} -- {{ champion.title }} //
+              {{ champion.tags }}</span
+            >
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import { mapActions, mapGetters } from 'vuex'
 export default {
-  async asyncData({ $axios, $config }) {
-    const champions = await $axios.$get(`/data/en_US/champion.json`)
-    return { champions }
+  computed: {
+    ...mapGetters({
+      champions: 'game/leagueoflegends/champions/getChampionList',
+    }),
+  },
+  created() {
+    this.getChampionList()
+  },
+  methods: {
+    ...mapActions({
+      getChampionList: 'game/leagueoflegends/champions/getChampionList',
+    }),
   },
 }
 </script>
