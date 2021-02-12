8ac5c260395a917e69e07f236c98acdcead7a3b0
mustafaeranil <mustafaeranil2@gmail.com>
2021-02-12 13:08:38 +0300

    Updated header , footer comp.

--- components/general/footer.vue
+++ components/general/footer.vue
@@ -1,9 +1,44 @@
 <template>
   <footer>
-    <div class="bg-gray-300">
-      <h1>Footer</h1>
-      <div class="text-center bg-gray-900 text-white">
-        ©{{ new Date().getFullYear() }}
+    <div class="bg-black py-20 w-full float-left">
+      <div class="container mx-auto">
+        <div class="grid grid-cols-7">
+          <div class="mb-10 col-span-3">
+            <NuxtLink to="/">
+              <img class="w-64 max-w-full" src="/footer_logo.svg" />
+            </NuxtLink>
+          </div>
+          <div class="col-span-2">
+            <div class="text-3xl font-bold text-primary mb-2 italic">Games</div>
+            <div>
+              <div v-for="game in gameLinks" :key="game.id">
+                <NuxtLink
+                  :to="game.link"
+                  class="font-bold text-gray-400 text-xs pr-3 py-2 flex items-center"
+                  >{{ game.name }}</NuxtLink
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-span-2">
+            <div class="text-3xl font-bold text-primary mb-2 italic">Pages</div>
+            <div>
+              <div v-for="page in pageLinks" :key="page.id">
+                <NuxtLink
+                  :to="page.link"
+                  class="font-bold text-gray-400 text-xs pr-3 py-2 flex items-center"
+                  >{{ page.name }}</NuxtLink
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-gray-900 mx-auto w-full float-left h-px"></div>
+    <div class="bg-black float-left w-full">
+      <div class="text-center text-gray-800 py-6 text-xs font-bold">
+        ©{{ new Date().getFullYear() }} GamePail - Tüm hakları saklıdır.
       </div>
     </div>
   </footer>
@@ -11,7 +46,16 @@
 
 <script>
 export default {
-  computed: {},
+  props: {
+    gameLinks: {
+      type: [Object, Array, JSON],
+      default: () => ({}),
+    },
+    pageLinks: {
+      type: [Object, Array, JSON],
+      default: () => ({}),
+    },
+  },
 }
 </script>
 
--- components/general/header.vue
+++ components/general/header.vue
@@ -1,14 +1,46 @@
 <template>
-  <div class="w-full text-center">
-    <div class="bg-black w-full h-8">
-      <div class="container mx-auto"></div>
+  <div class="w-full text-center z-10 relative">
+    <div class="bg-primary w-full float-left">
+      <div class="container mx-auto">
+        <div class="float-right flex items-stretch">
+          <div v-for="game in gameLinks" :key="game.id" class="float-left">
+            <template v-if="game.id === 'lol'">
+              <NuxtLink
+                :to="game.link"
+                class="font-black text-primary text-xs bg-white flex float-left px-3 py-2 flex items-center"
+              >
+                <img :src="'/navIcons/' + game.imgName" class="h-5 mr-1" />
+                <div class="float-left">{{ game.name }}</div></NuxtLink
+              >
+            </template>
+            <template v-else>
+              <NuxtLink
+                :to="game.link"
+                class="font-black text-white text-xs px-3 py-2 flex items-center"
+                >{{ game.name }}</NuxtLink
+              >
+            </template>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="w-full">
       <div class="container mx-auto">
-        <div class="w-full grid grid-cols-3 gap-4 items-center justify-between">
-          <div class="">LOGO</div>
-          <div class="">test</div>
-          <div class="">test</div>
+        <div class="w-full flex justify-between items-center flex-wrap">
+          <div class="float-left py-4">
+            <NuxtLink to="/">
+              <img src="/logo.svg" style="height: 45px" />
+            </NuxtLink>
+          </div>
+          <div class="float-right">
+            <div v-for="page in pageLinks" :key="page.id" class="float-left">
+              <NuxtLink
+                :to="page.link"
+                class="font-bold text-gray-900 text-sm px-3 py-2 flex items-center"
+                >{{ page.name }}</NuxtLink
+              >
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -16,7 +48,18 @@
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    gameLinks: {
+      type: [Object, Array, JSON],
+      default: () => ({}),
+    },
+    pageLinks: {
+      type: [Object, Array, JSON],
+      default: () => ({}),
+    },
+  },
+}
 </script>
 
 <style></style>
